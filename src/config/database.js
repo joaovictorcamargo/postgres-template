b27b1e93c12d5f20fb1b0b97def127a4efff7f65
joaovictor <jc7155004@gmail.com>
2022-02-26 12:41:12 -0300

    postgres

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "Lilica123!",
+  database: "yout",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
